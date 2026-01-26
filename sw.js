4eba62a55a216c6c24255160956d4f84904d6df8
edugrowth5-rgb <edugrowth5@gmail.com>
2026-01-26 15:33:13 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const cacheName = 'sagar-gold-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event: Files ko memory mein save karna
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Activate Event: Purana cache saaf karna
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Offline hone par bhi app chalana
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
